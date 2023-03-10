a9252c5151ccab231a6ad4e19a5f3eb60746d09e
hindurson <44705555+hindurson@users.noreply.github.com>
2023-03-10 20:36:16 +0000

    Itrating throuh arrays

--- src/App.java
+++ src/App.java
@@ -1,6 +1,31 @@
+import java.util.Arrays;
+
 public class App {
   
   public static void main(String[] args) {
-    
+    int[] arr = new int[] {92, 12, 38, 74};
+    for (int i = 0; i<arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    for (int i = arr.length-1; i >=0; i--) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    for (int i = arr.length - 1; i >= 0; i= i-2) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    for(int item : arr) {
+      System.out.println(item);
+    }
+    System.out.println();
+
+    //java Streams
+    Arrays.stream(arr).forEach(System.out::println);
+      
+    }
   }
-}
